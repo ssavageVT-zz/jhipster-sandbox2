ebe458e89bdcd4e0d4d84dc0f2c56d796dba2a48
ssavageVT <ssavage@gmail.com>
2016-09-26 10:17:30 -0400

    Update custom method for finding employee by email

--- src/main/java/tech/ipponusa/repository/EmployeeRepository.java
+++ src/main/java/tech/ipponusa/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package tech.ipponusa.repository;
 import tech.ipponusa.domain.Employee;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +14,9 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
+	@Query("From Employee e where e.email = :emailAddress")
+	@Transactional(readOnly=true)
+	public Employee findEmployeeByEmail(@Param("emailAddress") String emailAddress);
+	
+	
 }
--- src/main/java/tech/ipponusa/service/impl/PtoPeriodServiceImpl.java
+++ src/main/java/tech/ipponusa/service/impl/PtoPeriodServiceImpl.java
@@ -1,8 +1,13 @@
 package tech.ipponusa.service.impl;
 
 import tech.ipponusa.service.PtoPeriodService;
+import tech.ipponusa.domain.Employee;
 import tech.ipponusa.domain.PtoPeriod;
+import tech.ipponusa.domain.User;
+import tech.ipponusa.repository.EmployeeRepository;
 import tech.ipponusa.repository.PtoPeriodRepository;
+import tech.ipponusa.repository.UserRepository;
+import tech.ipponusa.security.SecurityUtils;
 import tech.ipponusa.service.dto.PtoPeriodDTO;
 import tech.ipponusa.service.mapper.PtoPeriodMapper;
 import org.slf4j.Logger;
@@ -11,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+
+import java.time.ZonedDateTime;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,6 +36,13 @@ public class PtoPeriodServiceImpl implements PtoPeriodService{
 
     @Inject
     private PtoPeriodMapper ptoPeriodMapper;
+    
+    @Inject 
+    private EmployeeRepository employeeRepository;
+   
+    
+    @Inject
+    private UserRepository userRepository;
 
     /**
      * Save a ptoPeriod.
@@ -37,11 +51,15 @@ public class PtoPeriodServiceImpl implements PtoPeriodService{
      * @return the persisted entity
      */
     public PtoPeriodDTO save(PtoPeriodDTO ptoPeriodDTO) {
-        log.debug("Request to save PtoPeriod : {}", ptoPeriodDTO);
-        PtoPeriod ptoPeriod = ptoPeriodMapper.ptoPeriodDTOToPtoPeriod(ptoPeriodDTO);
-        ptoPeriod = ptoPeriodRepository.save(ptoPeriod);
-        PtoPeriodDTO result = ptoPeriodMapper.ptoPeriodToPtoPeriodDTO(ptoPeriod);
-        return result;
+    	 log.debug("Request to save PtoPeriod : {}", ptoPeriodDTO);
+         PtoPeriod ptoPeriod = ptoPeriodMapper.ptoPeriodDTOToPtoPeriod(ptoPeriodDTO);
+         
+         
+         
+         
+         ptoPeriod = ptoPeriodRepository.save(ptoPeriod);
+         PtoPeriodDTO result = ptoPeriodMapper.ptoPeriodToPtoPeriodDTO(ptoPeriod);
+         return result;
     }
 
     /**
@@ -56,7 +74,34 @@ public class PtoPeriodServiceImpl implements PtoPeriodService{
             .map(ptoPeriodMapper::ptoPeriodToPtoPeriodDTO)
             .collect(Collectors.toCollection(LinkedList::new));
 
+        if(result.isEmpty()){
+        	PtoPeriodDTO init = new PtoPeriodDTO();
+        	init.setEndDate(calculatePtoPeriodEndDate());
+        	result.add(init);
+        }
+        
         return result;
+    }
+    
+    private ZonedDateTime calculatePtoPeriodEndDate(){
+    	
+    	//Modify the PTO period based on who is logged in.
+        String login = SecurityUtils.getCurrentUserLogin();
+        log.info("PTO Period Info");
+        log.info("User Requesting:"  + login);
+        
+        //Get the email address from the user table
+        //Use email address to lookup the employee ID
+        //From there we can determine the PTO period and check balances
+        User user = userRepository.findOneByLogin(login).get();
+        log.info("User Found: " + user.getFirstName() + " " + user.getLastName());
+        log.info("User Email Address: " + user.getEmail());       
+        
+        Employee employee = employeeRepository.findEmployeeByEmail(user.getEmail());
+    	
+        log.info("Employee Found: " + employee.getFirstName() + " " + employee.getLastName());
+        
+    	return employee.getHireDate().plusYears(1);
     }
 
     /**
